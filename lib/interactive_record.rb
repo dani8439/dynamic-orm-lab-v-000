6f7390176d3d475326c0c3640208111fac67021c
Dani Schuhman <dani8439@aol.com>
2018-08-06 18:05:14 +0000

    automatically backed up by learn

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -26,7 +26,7 @@ class InteractiveRecord
   end
 
   def initialize(options = {})
-    options.each do |property, value| 
+    options.each do |property, value|
       self.send("#{property}=", value)
     end
 
